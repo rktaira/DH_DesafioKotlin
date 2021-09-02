b03a8194d05d0576b685abbba394dfa76fd6c67e
rktaira <ricardoktaira@gmail.com>
2021-09-02 19:58:48 -0300

    Iniciando parte D

--- src/main/kotlin/DesafioKotlin/Professor.kt
+++ src/main/kotlin/DesafioKotlin/Professor.kt
@@ -1,4 +1,13 @@
 package DesafioKotlin
 
-class Professor {
+class Professor(
+    var nome: String,
+    var sobrenome: String,
+    var tempoDeCasa: Int,
+    var codigo: Int
+) {
+    //Sobrescrevendo equals para comparar o código de aluno.
+    override fun equals(other: Any?): Boolean {
+        return (other is Professor && other.codigo == this.codigo)
+    }
 }
