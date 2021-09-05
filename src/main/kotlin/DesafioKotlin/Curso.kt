27ac38821bba980ccbbe06cab90c30e8bcafadcc
rktaira <ricardoktaira@gmail.com>
2021-09-05 17:25:49 -0300

    Iniciando parte I

--- src/main/kotlin/DesafioKotlin/Curso.kt
+++ src/main/kotlin/DesafioKotlin/Curso.kt
@@ -7,14 +7,38 @@ class Curso(
     var profAdjunto: ProfessorAdjunto,
     val alunosMax: Int
 ) {
-    var listaAlunos: List<Aluno> = listOf()
+    var listaAlunos: MutableList<Aluno> = mutableListOf()
 
     //Sobrescrevendo equals para comparar o código de curso.
     override fun equals(other: Any?): Boolean {
-        return(other is Curso && other.codigo == this.codigo)
+        return (other is Curso && other.codigo == this.codigo)
     }
 
-    
+    fun adicionarUmAluno(umAluno: Aluno): Boolean {
+        if (listaAlunos.contains(umAluno) == true) {
+            println("Erro: Aluno já está matriculado neste curso.")
+            return false
+        } else if (listaAlunos.size >= alunosMax) {
+            println("Erro: Número máximo de alunos já foi atingido.")
+            return false
+        } else {
+            listaAlunos.add(umAluno)
+            println("Aluno incluído com sucesso!")
+            return true
+        }
+    }
+
+    fun excluirAluno(umAluno: Aluno): Boolean {
+        if (listaAlunos.contains(umAluno) == false) {
+            println("Erro: Aluno não está matriculado neste curso.")
+            return false
+        } else {
+            listaAlunos.remove(umAluno)
+            println("Aluno excluído com sucesso!")
+            return true
+        }
+    }
 }
+
 
 //Parte E, 1) Adicionar os parâmetros profTitular, profAdjunto, maxAlunos e listaAlunos.
--- src/main/kotlin/DesafioKotlin/DigitalHouseManager.kt
+++ src/main/kotlin/DesafioKotlin/DigitalHouseManager.kt
@@ -5,4 +5,5 @@ class DigitalHouseManager(
     professores: List<Professor>,
     cursos: List<Curso>
 ) {
+    
 }
